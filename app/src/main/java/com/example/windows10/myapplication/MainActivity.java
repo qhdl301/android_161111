4254a21db21ab9bfb1d9d71988f4812cb1c12a0e
Young soo Kim <qhdl301@gmail.com>
2016-11-11 12:23:13 +0900

    create star event, button event

--- app/src/main/java/com/example/windows10/myapplication/MainActivity.java
+++ app/src/main/java/com/example/windows10/myapplication/MainActivity.java
@@ -2,12 +2,52 @@ package com.example.windows10.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+     Button b1;
+     Button b2;
+     RatingBar RatingBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "안녕?",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        b2=(Button)findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "잘가!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RatingBar=(RatingBar)findViewById(R.id.ratingBar);
+
+        RatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                Toast.makeText(getApplicationContext(),
+                        ("평점은:"+ratingBar.getRating()),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+
     }
 }
